97036734ae1f7684a9f60d1ff35b10e744fc398a
Ji Yim <gsooyim@gmail.com>
2018-04-19 17:12:06 +0000

    automatically backed up by learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -8,8 +8,8 @@ def happy_birthday(birthday_kids)
   def happy_birthday(birthday_kids)
     birthday_kids.each do |kids_name, age|
       puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+      end
     end
-  end
 end
 
 
